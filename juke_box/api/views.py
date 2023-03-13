c3c54b6cb2b36c719a4777f5ef3702fb4508e99c
ngc.918 <nicolas.g9182@gmail.com>
2023-03-12 23:44:42 -0400

    first commit

---
+++ juke_box/api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Room
+from .serialziers import RoomSerializer
+
+# Create your views here.
+
+'''View and create different rooms'''
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
